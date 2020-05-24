92afb210149fcbbc1b3a2493f3618a0c8a94a277
Alex Magee <alex.magee16701@gmail.com>
2020-05-24 12:38:38 -0400

    Update index.js
    bye bye switch statement 😂 doesnt work too well with this bot i guess

--- index.js
+++ index.js
@@ -17,38 +17,32 @@ client.once('ready', () => {
 
 client.on('message', async message => {
     
-    var chatmsg = message.content;
-    var thisMsg = chatmsg.toLowerCase();
+    var chatmsg = message.content.toLowerCase();
     const serverQueue = queue.get(message.guild.id);
 
-    switch (thisMsg) { 
-        //=help or =commands
-        case (thisMsg.startsWith(`${prefix}help`) | thisMsg.startsWith(`${prefix}commands`)) :
+    try { 
+        if (chatmsg.startsWith(`${prefix}help`) | chatmsg.startsWith(`${prefix}commands`)) {
             message.channel.send("To view the complete list of commands, visit the Github page!\n<https://github.com/CTM-GOAT/Internet-Explorer-Bot/blob/master/README.md>");
-        break;
-        //=invite
-        case (thisMsg.startsWith(`${prefix}invite`)) : 
+        };
+        if (chatmsg.startsWith(`${prefix}invite`)) {
             message.channel.send("https://discordapp.com/oauth2/authorize?client_id=667197788441804815&scope=bot&permissions=3660864");
-        break;
-        //=play
-        case (thisMsg.startsWith(`${prefix}play`)) : 
+        };
+        if (chatmsg.startsWith(`${prefix}play`)) {
             var TextInMsg = chatmsg.substring(5);
             if (TextInMsg.length > 0)  {
                 execute(message, serverQueue);
-            };
-        break;
-        //=skip
-        case (thisMsg.startsWith(`${prefix}skip`)) : 
+            }
+        };
+        if (chatmsg.startsWith(`${prefix}skip`)) {
             skip(message, serverQueue);
-        break;
-        //=stop
-        case (thisMsg.startsWith(`${prefix}stop`)) : 
+        };
+        if (chatmsg.startsWith(`${prefix}stop`)) {
             stop(message, serverQueue);
-        break;
-        //=img
-        case (thisMsg.startsWith(`${prefix}img`)) : 
+        };
+        
+        if (chatmsg.toLowerCase().startsWith(`${prefix}img`)) {
             var search = chatmsg.substring(5);
-            if (search.length > 0) { 
+           if (search.length > 0) { 
             var options = {
                 url: "http://results.dogpile.com/serp?qc=images&q=" + search,
                 method: "GET",
@@ -59,7 +53,6 @@ client.on('message', async message => {
             };
             request(options, function(error, response, responseBody) {
                 if (error) {
-                    // handle error
                     return;
                 }
                 $ = cheerio.load(responseBody);
@@ -67,32 +60,28 @@ client.on('message', async message => {
                 var urls = new Array(links.length).fill(0).map((v, i) => links.eq(i).attr("href"));
                 console.log(urls);
                 if (!urls.length) {
-                    // Handle no results
                     return;
                 }
                 message.channel.send( urls[0] );
             });
-            } 
-            else { 
+           } 
+           else { 
             message.channel.send("Error: no search term provided.  Usage example: =img apples");
-            }
-        break;
-        //=echo
-        case (thisMsg.startsWith(`${prefix}echo`)) : 
+           }
+        };
+        if (chatmsg.toLowerCase().startsWith(`${prefix}echo`)) {
             var echomsg = chatmsg.substring(6).replace('=', '');
             if (echomsg.length > 0) {
                 message.channel.send(echomsg);
             } else {
                 message.channel.send("Error: nothing to echo.  Usage example: $echo The new Tesla Cybertruck is cool.");
-            };
-        break;
-        //=roll
-        case (thisMsg.startsWith(`${prefix}roll`)) : 
+            }
+        };
+        if (chatmsg.toLowerCase().startsWith(`${prefix}roll`)) {
             var randnum = Math.floor(Math.random() * (100 - 1) + 1);
             message.channel.send("🎲 " + randnum);
-        break;
-        //=chance
-        case (thisMsg.startsWith(`${prefix}chance`)) : 
+        };
+        if (chatmsg.toLowerCase().startsWith(`${prefix}chance`)) {
             var randnum = Math.floor(Math.random() * (100 - 1) + 1);
             var isLikely = "not likely";
             var isLikelyEmoji = "🙅‍♀️";
@@ -101,191 +90,191 @@ client.on('message', async message => {
                 isLikelyEmoji = "👌";
             }
             message.channel.send(randnum + "% chance, its " + isLikely + ". " + isLikelyEmoji);
-        break;
-        //=source
-        case (thisMsg.startsWith(`${prefix}source`) | chatmsg.startsWith(`${prefix}source code`)) : 
+        };
+        if (chatmsg.toLowerCase().startsWith(`${prefix}source`) | chatmsg.startsWith(`${prefix}source code`)) {
             message.channel.send("https://github.com/CTM-GOAT/Internet-Explorer-Bot");
-        break;
-        //=owo
-        case (thisMsg.startsWith(`${prefix}owo`)) : 
+        };
+        if (chatmsg.toLowerCase().startsWith(`${prefix}owo`)) {
             var echomsg = chatmsg.substring(5).replace(new RegExp('l', 'g'), 'w').replace(new RegExp('r', 'g'), 'w');
             if (echomsg.length > 0) {
                 message.channel.send(echomsg.replace(new RegExp('$', 'g'), '') + "  💦");
             } else {
                 message.channel.send("Error: nothing to owo-ify.  Usage example: $owo Hit or miss, I guess they never miss, huh?.");
             }
-        break;
-        //=ban
-        case (thisMsg.startsWith(`${prefix}ban`)) : 
+        };
+        if (chatmsg.toLowerCase().startsWith(`${prefix}ban`)) {
             var echomsg = chatmsg.substring(5);
             if (echomsg.length > 0) {
                 message.channel.send(chatmsg.substring(5) + " has been banned! 🔨");
             } else {
                 message.channel.send("Error: noone to ban.  Usage example: $ban alex");
             }
-        break;
-        case (thisMsg.startsWith(`${prefix}hl3`)) : 
+        };
+        if (chatmsg.toLowerCase().startsWith(`${prefix}hl3`)) {
             message.channel.send("\n.             " +
-            "           ⢀⣀⣠⣤⣤⣴⣦⣤⣤⣄⣀\n" +
-            "⠀⠀⠀⠀⠀⠀⢀⣤⣾⣿⣿⣿⣿⠿⠿⠿⠿⣿⣿⣿⣿⣶⣤⡀\n" +
-            "⠀⠀⠀⠀⣠⣾⣿⣿⡿⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⢿⣿⣿⣶⡀\n" +
-            "⠀⠀⠀⣴⣿⣿⠟⠁⠀⠀⠀⣶⣶⣶⣶⡆⠀⠀⠀⠀⠀⠀⠈⠻⣿⣿⣦\n" +
-            "⠀⠀⣼⣿⣿⠋⠀⠀⠀⠀⠀⠛⠛⢻⣿⣿⡀⠀⠀⠀⠀⠀⠀⠀⠙⣿⣿⣧\n" +
-            "⠀⢸⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⣿⣷⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⣿⡇\n" +
-            "⠀⣿⣿⡿⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⣿⣿⣿⣇⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿\n" +
-            "⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⡟⢹⣿⣿⡆⠀⠀⠀⠀⠀⠀⠀⣹⣿⣿\n" +
-            "⠀⣿⣿⣷⠀⠀⠀⠀⠀⠀⣰⣿⣿⠏⠀⠀⢻⣿⣿⡄⠀⠀⠀⠀⠀⠀⣿⣿⡿\n" +
-            "⠀⢸⣿⣿⡆⠀⠀⠀⠀⣴⣿⡿⠃⠀⠀⠀⠈⢿⣿⣷⣤⣤⡆⠀⠀⣰⣿⣿⠇\n" +
-            "⠀⠀⢻⣿⣿⣄⠀⠀⠾⠿⠿⠁⠀⠀⠀⠀⠀⠘⣿⣿⡿⠿⠛⠀⣰⣿⣿⡟\n" +
-            "⠀⠀⠀⠻⣿⣿⣧⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿⣿⠏\n" +
-            "⠀⠀⠀⠀⠈⠻⣿⣿⣷⣤⣄⡀⠀⠀⠀⠀⠀⠀⢀⣠⣴⣾⣿⣿⠟⠁\n" +
-            "⠀⠀⠀⠀⠀⠀⠈⠛⠿⣿⣿⣿⣿⣿⣶⣶⣿⣿⣿⣿⣿⠿⠋⠁\n" +
-            "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠛⠛⠛⠛⠛⠛⠉⠉\n"
-        );
-        break;
-        case (thisMsg.startsWith(`${prefix}xd`)) : 
+                "           ⢀⣀⣠⣤⣤⣴⣦⣤⣤⣄⣀\n" +
+                "⠀⠀⠀⠀⠀⠀⢀⣤⣾⣿⣿⣿⣿⠿⠿⠿⠿⣿⣿⣿⣿⣶⣤⡀\n" +
+                "⠀⠀⠀⠀⣠⣾⣿⣿⡿⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⢿⣿⣿⣶⡀\n" +
+                "⠀⠀⠀⣴⣿⣿⠟⠁⠀⠀⠀⣶⣶⣶⣶⡆⠀⠀⠀⠀⠀⠀⠈⠻⣿⣿⣦\n" +
+                "⠀⠀⣼⣿⣿⠋⠀⠀⠀⠀⠀⠛⠛⢻⣿⣿⡀⠀⠀⠀⠀⠀⠀⠀⠙⣿⣿⣧\n" +
+                "⠀⢸⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⣿⣷⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⣿⡇\n" +
+                "⠀⣿⣿⡿⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⣿⣿⣿⣇⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿\n" +
+                "⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⡟⢹⣿⣿⡆⠀⠀⠀⠀⠀⠀⠀⣹⣿⣿\n" +
+                "⠀⣿⣿⣷⠀⠀⠀⠀⠀⠀⣰⣿⣿⠏⠀⠀⢻⣿⣿⡄⠀⠀⠀⠀⠀⠀⣿⣿⡿\n" +
+                "⠀⢸⣿⣿⡆⠀⠀⠀⠀⣴⣿⡿⠃⠀⠀⠀⠈⢿⣿⣷⣤⣤⡆⠀⠀⣰⣿⣿⠇\n" +
+                "⠀⠀⢻⣿⣿⣄⠀⠀⠾⠿⠿⠁⠀⠀⠀⠀⠀⠘⣿⣿⡿⠿⠛⠀⣰⣿⣿⡟\n" +
+                "⠀⠀⠀⠻⣿⣿⣧⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿⣿⠏\n" +
+                "⠀⠀⠀⠀⠈⠻⣿⣿⣷⣤⣄⡀⠀⠀⠀⠀⠀⠀⢀⣠⣴⣾⣿⣿⠟⠁\n" +
+                "⠀⠀⠀⠀⠀⠀⠈⠛⠿⣿⣿⣿⣿⣿⣶⣶⣿⣿⣿⣿⣿⠿⠋⠁\n" +
+                "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠛⠛⠛⠛⠛⠛⠉⠉\n"
+            );
+        };
+        if (chatmsg.toLowerCase().startsWith(`${prefix}xd`)) {
             message.channel.send("X D", {
                 tts: true
             })
-        break;
-        //=sadcat
-        case (thisMsg.startsWith(`${prefix}sadcat`)) : 
+        };
+        if (chatmsg.toLowerCase().startsWith(`${prefix}sadcat`)) {
             message.channel.send({
                 files: ["https://i.kym-cdn.com/photos/images/original/001/389/465/663.jpg"]
             });
-        break;
-        //=pcmr
-        case (thisMsg.startsWith(`${prefix}pcmr`)) : 
+        };
+        if (chatmsg.toLowerCase().startsWith(`${prefix}pcmr`)) {
             loadPcmrPosts();
-        break;
-        //=chrome
-        case (thisMsg.includes(`chrome`)) : 
-            message.channel.send('We don\'t mention that browser here.');
-        break;
-        case (thisMsg.includes(`internet explorer`)) : 
-            message.channel.send('What the hell do you want??');
-        break;
-        case (thisMsg.includes(`arch`)) : 
+        };
+        if (chatmsg.toLowerCase().includes(`chrome`)) {
+            message.channel.send('We don\'t mention that browser here...');
+        };
+        if (chatmsg.toLowerCase().includes(`internet explorer`)) {
+            message.channel.send('What the hell do you want from me??');
+        };
+        if (chatmsg.toLowerCase().includes(`microsoft edge`)) {
+            message.channel.send('I CAN KICK MS EDGE\'S ASSSSS DONT FUUUUCK WITH ME');
+        };
+        if (chatmsg.toLowerCase().includes(`arch`)) {
             message.channel.send('One time I was ordering coffee and suddenly realised the barista didn\'t know I use A r c h. Needless to say, I stopped mid-order to inform her that I do indeed use A r c h. I must have spoken louder than I intended because the whole café instantly erupted into a prolonged applause. I walked outside with my head held high. I never did finish my order that day, but just knowing that everyone around me was aware that I use A r c h was more energising than a simple cup of coffee could ever be.');
-        break;
-        case (thisMsg.includes("69")) : 
+        };
+        if (chatmsg.includes("69")) {
             message.channel.send("Nice.");
-        break;
-        case (thisMsg.includes("spam")) : 
+        };
+        if (chatmsg.toLowerCase().includes("spam")) {
             message.channel.send("Did");
             message.channel.send("someone");
             message.channel.send("say");
             message.channel.send("s p a m ?");
-        break;
-        case (thisMsg.includes("nudes")) : 
+        };
+        if (chatmsg.toLowerCase().includes("nudes")) {
             message.channel.send({
                 files: ["./Internet_Explorer_7.png"]
             });
             message.channel.send("***Do you like what you see?***");
-        break;
-        case (thisMsg.includes("gamer")) : 
-            message.channel.send("You think you're a real gamer, but you won't rise at 330am to get those extra hours of grinding in. I have pre packaged meals on-go for any situation. I've got weed to bury the pain of my carpel tunnel. I've gone through about 17 different logitech and corsair keyboards. 23 mouses, and 5 monitors later I feel like I can take on anybody and anything. I only sleep once my quests have been fulfilled, and my E-girlfriend has been supporting me along the way, even if I've never heard her voice before I know she wants the best for me. All it takes is one little assumption about me for me to completely obliterate any chance of you beating me. I can use black magic to reverse your energy, completely polarising your ability to use hand eye coordination. Dont fuck with me, boomer.");
-        break;
-        default: 
-        break;
-    }
-
-    async function execute(message, serverQueue) {
-        const args = message.content.split(' ');
-    
-        const voiceChannel = message.member.voiceChannel;
-        if (!voiceChannel) return message.channel.send('You need to be in a voice channel to play music!');
-        const permissions = voiceChannel.permissionsFor(message.client.user);
-        if (!permissions.has('CONNECT') || !permissions.has('SPEAK')) {
-            return message.channel.send('I need the permissions to join and speak in your voice channel!');
-        }
-    
-        const songInfo = await ytdl.getInfo(args[1]);
-        const song = {
-            title: songInfo.title,
-            url: songInfo.video_url,
         };
-    
-        if (!serverQueue) {
-            const queueContruct = {
-                textChannel: message.channel,
-                voiceChannel: voiceChannel,
-                connection: null,
-                songs: [],
-                volume: 5,
-                playing: true,
-            };
-    
-            queue.set(message.guild.id, queueContruct);
-    
-            queueContruct.songs.push(song);
-    
-            try {
-                var connection = await voiceChannel.join();
-                queueContruct.connection = connection;
-                play(message.guild, queueContruct.songs[0]);
-            } catch (err) {
-                console.log(err);
-                queue.delete(message.guild.id);
-                return message.channel.send(err);
+        if (chatmsg.toLowerCase().includes("gamer")) {
+            message.channel.send("You think you're a real g a m e r, but you won't rise at 330am to get those extra hours of grinding in. I have pre packaged meals on-go for any situation. I've got weed to bury the pain of my carpel tunnel. I've gone through about 17 different logitech and corsair keyboards. 23 mice, and 5 monitors later I feel like I can take on anybody and anything. I only sleep once my quests have been fulfilled, and my E-girlfriend has been supporting me along the way, even if I've never heard her voice before I know she wants the best for me. All it takes is one little assumption about me for me to completely obliterate any chance of you beating me. I can use black magic to reverse your energy, completely polarising your ability to use hand eye coordination. Dont fuck with me, boomer.");
+        }
+        async function execute(message, serverQueue) {
+            const args = message.content.split(' ');
+        
+            const voiceChannel = message.member.voiceChannel;
+            if (!voiceChannel) return message.channel.send('You need to be in a voice channel to play music!');
+            const permissions = voiceChannel.permissionsFor(message.client.user);
+            if (!permissions.has('CONNECT') || !permissions.has('SPEAK')) {
+                return message.channel.send('I need the permissions to join and speak in your voice channel!');
             }
-        } else {
-            serverQueue.songs.push(song);
-            console.log(serverQueue.songs);
-            return message.channel.send(`${song.title} has been added to the queue!`);
+        
+            const songInfo = await ytdl.getInfo(args[1]);
+            const song = {
+                title: songInfo.title,
+                url: songInfo.video_url,
+            };
+        
+            if (!serverQueue) {
+                const queueContruct = {
+                    textChannel: message.channel,
+                    voiceChannel: voiceChannel,
+                    connection: null,
+                    songs: [],
+                    volume: 5,
+                    playing: true,
+                };
+        
+                queue.set(message.guild.id, queueContruct);
+        
+                queueContruct.songs.push(song);
+        
+                try {
+                    var connection = await voiceChannel.join();
+                    queueContruct.connection = connection;
+                    play(message.guild, queueContruct.songs[0]);
+                } catch (err) {
+                    console.log(err);
+                    queue.delete(message.guild.id);
+                    return message.channel.send(err);
+                }
+            } else {
+                serverQueue.songs.push(song);
+                console.log(serverQueue.songs);
+                return message.channel.send(`${song.title} has been added to the queue!`);
+            }
+        
+        }
+        
+        function skip(message, serverQueue) {
+            if (!message.member.voiceChannel) return message.channel.send('You have to be in a voice channel to stop the music!');
+            if (!serverQueue) return message.channel.send('There is no song that I could skip!');
+            serverQueue.connection.dispatcher.end();
+        }
+        
+        function stop(message, serverQueue) {
+            if (!message.member.voiceChannel) return message.channel.send('You have to be in a voice channel to stop the music!');
+            serverQueue.songs = [];
+            serverQueue.connection.dispatcher.end();
+        }
+        
+        function play(guild, song) {
+            const serverQueue = queue.get(guild.id);
+        
+            if (!song) {
+                serverQueue.voiceChannel.leave();
+                queue.delete(guild.id);
+                return;
+            }
+        
+            const dispatcher = serverQueue.connection.playStream(ytdl(song.url))
+                .on('end', () => {
+                    console.log('Music ended!');
+                    serverQueue.songs.shift();
+                    play(guild, serverQueue.songs[0]);
+                })
+                .on('error', error => {
+                    console.error(error);
+                });
+            dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
         }
     
+        function loadPcmrPosts() {
+            fetch('https://www.reddit.com/r/pcmasterrace.json?limit=100&?sort=top&t=all')
+              .then(res => res.json())
+              .then(json => json.data.children.map(v => v.data.url))
+              .then(urls => postPcmrPostRand(urls));
+        };
+          
+        function postPcmrPostRand(urls) {
+        const randomURL = urls[Math.floor(Math.random() * urls.length) + 1];
+        const embed = new Discord.RichEmbed({
+            image: {
+            url: randomURL
+            }
+        });
+        message.channel.send(embed);
+        };
+
     }
-    
-    function skip(message, serverQueue) {
-        if (!message.member.voiceChannel) return message.channel.send('You have to be in a voice channel to stop the music!');
-        if (!serverQueue) return message.channel.send('There is no song that I could skip!');
-        serverQueue.connection.dispatcher.end();
-    }
-    
-    function stop(message, serverQueue) {
-        if (!message.member.voiceChannel) return message.channel.send('You have to be in a voice channel to stop the music!');
-        serverQueue.songs = [];
-        serverQueue.connection.dispatcher.end();
-    }
-    
-    function play(guild, song) {
-        const serverQueue = queue.get(guild.id);
-    
-        if (!song) {
-            serverQueue.voiceChannel.leave();
-            queue.delete(guild.id);
-            return;
-        }
-    
-        const dispatcher = serverQueue.connection.playStream(ytdl(song.url))
-            .on('end', () => {
-                console.log('Music ended!');
-                serverQueue.songs.shift();
-                play(guild, serverQueue.songs[0]);
-            })
-            .on('error', error => {
-                console.error(error);
-            });
-        dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+    catch (err) { 
+        message.channel.send('***Oops, got an error:  \n***`' + err + '`');
     }
 
-    function loadPcmrPosts() {
-        fetch('https://www.reddit.com/r/pcmasterrace.json?limit=100&?sort=top&t=all')
-          .then(res => res.json())
-          .then(json => json.data.children.map(v => v.data.url))
-          .then(urls => postPcmrPostRand(urls));
-    };
-      
-    function postPcmrPostRand(urls) {
-    const randomURL = urls[Math.floor(Math.random() * urls.length) + 1];
-    const embed = new Discord.RichEmbed({
-        image: {
-        url: randomURL
-        }
-    });
-    message.channel.send(embed);
-    };
+
 
 });
 
